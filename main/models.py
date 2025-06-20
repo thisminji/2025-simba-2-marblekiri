748d343e9fc6459a7305f98ef31496291cecd622
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2025-06-21 00:08:23 +0900

    [BE] feat: 지정 턴 수 및 자동 종료 / 결과 요약 db 반영 / 턴 수, 현재 턴 사람 표시 기능 추가

--- main/models.py
+++ main/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 class GameRoom(models.Model):
     theme = models.CharField(max_length=30)
-    max_turns = models.PositiveIntegerField(null=True, blank=True)
     started = models.BooleanField(default=False)
-    current_turn_index = models.PositiveIntegerField(default=0) #턴 관리
+    max_turns = models.PositiveIntegerField(null=True, blank=True) #null 일 때 무제한
+    current_turn_index = models.PositiveIntegerField(default=0) #턴 관리 (1번님 턴 - 2번님 턴)
+    current_round = models.PositiveIntegerField(default=1)      # 바퀴 수 (1라운드 - 2라운드)
+    started_at = models.DateTimeField(auto_now_add=True)  # 게임 시작 시간 저장
 
 class PlayerInRoom(models.Model):
     nickname = models.CharField(max_length=30, default="익명")
     room = models.ForeignKey(GameRoom, on_delete=models.CASCADE)
     turn = models.PositiveIntegerField()
     is_ready = models.BooleanField(default=False)
-    drink_count = models.PositiveIntegerField(default=0)  # 카운트용
+    drink_count = models.PositiveIntegerField(default=0)  # 마시기 카운트용
 
 class Question(models.Model):
     THEME_CHOICES = [
