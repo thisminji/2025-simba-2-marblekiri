c6f97e515ba76e6676f391d1634a3d4c24948429
Yeonhee-Ha <flfkrdl123@gmail.com>
2025-06-20 14:56:00 +0900

    [BE] style: 파일 구조 수정 및 url 수정

--- marblekiri/urls.py
+++ marblekiri/urls.py
@@ -20,11 +20,25 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    #########################################################
+    # opening 
     path('', views.start_page, name="start"),
-    path('setup/', views.setup_page, name="setup"),
     path('game_start/', views.game_start, name='game_start'),
+    
+    ########################################################
+    #setting
+    path('setup/', views.setup_page, name="setup"),
+    
+    ########################################################
+    #game 
     path('game/', views.game_page, name="game"),
-    path('game/page/', views.game_page, name="game_page"),
+    
+    #######################################################
+    #custome
     path('custom_questions/', views.custom_questions, name="custom_questions"),
-    path('result/', views.result_page, name="result")
+    
+    #######################################################
+    #result
+    path('result/', views.result_page, name="result"),
 ]
