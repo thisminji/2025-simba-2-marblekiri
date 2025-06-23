d8b31cebd23cdeba3763a8d06822aad833ce9677
osein607 <osein607@naver.com>
2025-06-24 01:56:10 +0900

    [BE] style: result 프레임 스타일 수정

--- marblekiri/static/js/result.js
+++ marblekiri/static/js/result.js
@@ -1,16 +1,18 @@
 function previewImage(event) {
     const input = event.target;
     const preview = document.getElementById("preview");
-    const placeholder = document.getElementById("placeholder-text");
+    const placeholder = document.getElementById("placeholder-box");
     const removeBtn = document.getElementById("remove-image");
+    const imageLabel = document.getElementById("image-label");
 
     if (input.files && input.files[0]) {
     const reader = new FileReader();
     reader.onload = function (e) {
         preview.src = e.target.result;
-        preview.style.display = "block";
-        if (placeholder) placeholder.style.display = "none";
-        if (removeBtn) removeBtn.style.display = "inline-block";
+        preview.style.display = "block"; // 이미지 보이기
+        if (placeholder) placeholder.style.display = "none"; // 텍스트 숨기기
+        if (removeBtn) removeBtn.style.display = "block"; // 삭제 버튼 보이기
+        if (imageLabel) imageLabel.style.pointerEvents = "none"; // 더 이상 업로드 못하게
     };
     reader.readAsDataURL(input.files[0]);
     }
@@ -19,14 +21,16 @@ function previewImage(event) {
 function removeImage() {
     const input = document.getElementById("image-upload");
     const preview = document.getElementById("preview");
-    const placeholder = document.getElementById("placeholder-text");
+    const placeholder = document.getElementById("placeholder-box");
     const removeBtn = document.getElementById("remove-image");
+    const imageLabel = document.getElementById("image-label");
 
     input.value = ""; // 선택된 파일 제거
     preview.src = "";
     preview.style.display =  "none";
-    if (placeholder) placeholder.style.display = "block";
+    if (placeholder) placeholder.style.display = "flex";
     if (removeBtn) removeBtn.style.display = "none";
+    if (imageLabel) imageLabel.style.pointerEvents = "auto";
 }
 /////////////////////////////////////////////////////////////
 
